f53d96b6b7ac555ebdcb386cbd57cda74bedeacc
JackyYanSquare <aa1986779407@163.com>
2018-03-01 22:25:44 +0800

    day01 PictureLoader

--- app/src/main/java/com/innvc/drysister/PictureLoader.java
+++ app/src/main/java/com/innvc/drysister/PictureLoader.java
@@ -1,11 +1,80 @@
 package com.innvc.drysister;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * @author: Jacky
  * @date : 2018/3/1 21:59
  * @Email : aa1986779407@163.com
- * @description:
+ * @description: 下载加载图片的类
+ * 线程将输入流转成字节数组,转换完毕 handler 里 decodeByteArray 将字节数组解码成bitmap
  */
 public class PictureLoader {
-    
+    private String    imgUrl;
+    private ImageView loadImg;
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 0x123) {
+                if (picByte != null) {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(picByte, 0, picByte.length);
+                    loadImg.setImageBitmap(bitmap);
+                }
+            }
+        }
+    };
+
+    public void load(ImageView loadImg, String imgUrl) {
+        this.loadImg = loadImg;
+        this.imgUrl = imgUrl;
+        Drawable drawable = loadImg.getDrawable();
+        if (drawable != null && drawable instanceof BitmapDrawable) {
+            Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+            if (bitmap != null && !bitmap.isRecycled()) {
+                bitmap.recycle();
+            }
+        }
+        new Thread(mRunnable).start();
+    }
+
+    private byte[] picByte;
+    Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                URL url = new URL(imgUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setReadTimeout(10000);
+                if (conn.getResponseCode() == 200) {
+                    InputStream in = conn.getInputStream();
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[10240];
+                    int length = -1;
+                    while ((length = in.read(buffer)) != -1) {
+                        out.write(buffer, 0, length);
+                    }
+                    picByte = out.toByteArray();
+                    in.close();
+                    out.close();
+                    mHandler.sendEmptyMessage(0x123);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
 }
