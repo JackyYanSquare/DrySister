a0c0521d513ed21642a53b7a53e1afefd1c0a5d7
JackyYanSquare <aa1986779407@163.com>
2018-03-07 17:55:21 +0800

    下班

--- app/src/main/java/com/innvc/drysister/ui/activity/MainActivity.java
+++ app/src/main/java/com/innvc/drysister/ui/activity/MainActivity.java
@@ -1,11 +1,17 @@
 package com.innvc.drysister.ui.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.innvc.drysister.R;
 import com.innvc.drysister.bean.entity.Sister;
@@ -39,13 +45,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(MainActivity.this, Manifest
+                .permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission
+                    .WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        } else {
 
-        sisterApi = new SisterApi();
-        loader = new PictureLoader();
-        mLoader = SisterLoader.getInstance(MainActivity.this);
-        mLoader.bindBitmap(data.get(curPos).getUrl(), showImg, 400, 400);
-        initData();
-        initUI();
+            sisterApi = new SisterApi();
+            // loader = new PictureLoader();
+            // mLoader = SisterLoader.getInstance(MainActivity.this);
+            // mLoader.bindBitmap(data.get(curPos).getUrl(), showImg, 400, 400);
+            initData();
+            initUI();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[]
+            grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initData();
+                    initUI();
+                } else {
+                    Toast.makeText(this, "haha", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
     }
 
     @Override
@@ -56,6 +85,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initData() {
         data = new ArrayList<>();
+        mLoader = SisterLoader.getInstance(MainActivity.this);
         // new SisterTask(page).execute();  // 该方法可能导致内存溢出
         /*data.add("http://ww4.sinaimg.cn/large/610dc034jw1f6ipaai7wgj20dw0kugp4.jpg");
         data.add("http://ww3.sinaimg.cn/large/610dc034jw1f6gcxc1t7vj20hs0hsgo1.jpg");
@@ -75,7 +105,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         refreshBtn = findViewById(R.id.btn_change);
         showBtn.setOnClickListener(this);
         refreshBtn.setOnClickListener(this);
-
+        mLoader.bindBitmap(data.get(curPos).getUrl(), showImg, 400, 400);
     }
 
     @Override
